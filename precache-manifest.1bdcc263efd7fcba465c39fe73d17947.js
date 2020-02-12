dd00dfa8012671527b7b8254c695e8a2e49c92ff
Ritesh Ranjan <ritranjan@deloitte.com>
2020-02-13 01:56:30 +0530

    Updates

---
+++ precache-manifest.1bdcc263efd7fcba465c39fe73d17947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad684dbc424a9ca6e1453ba64289cdc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d1f930f74d38965fbf4",
+    "url": "/static/js/2.17638d86.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/static/js/2.17638d86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cf71d0788303da9dba5",
+    "url": "/static/js/main.20e618f8.chunk.js"
+  },
+  {
+    "revision": "d15bf3493322771e549e",
+    "url": "/static/js/runtime-main.98de51ec.js"
+  },
+  {
+    "revision": "8fd984d96fe78db0e96b3ed028f46028",
+    "url": "/static/media/me.8fd984d9.jpg"
+  }
+]);
